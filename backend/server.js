ef536de9d4cf714d386507c6500804a85afd642b
Ridwan <ridwanwiseman@gmail.com>
2026-05-06 12:17:53 +0100

    update all URLs to www.tellher.co

--- backend/server.js
+++ backend/server.js
@@ -721,7 +721,7 @@ app.get('/success', async (req, res) => {
     }
   }
 
-  res.redirect('https://tell-her-production.up.railway.app/?payment=success#login');
+  res.redirect('https://www.tellher.co/?payment=success#login');
 });
 
 // ── User Profile Get ──────────────────────────────────
